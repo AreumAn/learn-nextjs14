c15711e4a1c9b360cf70ebceb96de93a39b3b3de
Areum An <areum.an.0928@gmail.com>
2024-10-12 19:30:18 -0400

    metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,8 @@
+
+export const metadata = {
+  title: 'Home'
+}
+
 export default function Page() {
   return <div>
     <h1>Main!</h1>
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'About Us'
+}
+
 export default function AboutUs() {
   return <div>
     <h1>about us</h1>
