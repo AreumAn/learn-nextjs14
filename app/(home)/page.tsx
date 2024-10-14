c9d612b3d35cf642c1bdf1a7dff377777c682f9f
Areum An <areum.an.0928@gmail.com>
2024-10-14 13:15:36 -0400

    Loading components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,8 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovives() {
+  // temp add to see Loading
+  await new Promise((resolve) => setTimeout(resolve, 5000))
   return fetch(URL).then(response => response.json())
 }
 
