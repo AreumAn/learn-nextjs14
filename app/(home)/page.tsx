01afc1fdde753d39b6a14336c4a66664d38ec0b5
Areum An <areum.an.0928@gmail.com>
2024-10-14 16:22:15 -0400

    parallel request

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,22 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Home'
 }
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovives() {
   // temp add to see Loading
-  await new Promise((resolve) => setTimeout(resolve, 5000))
-  return fetch(URL).then(response => response.json())
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return fetch(API_URL).then(response => response.json())
 }
 
 export default async function Page() {
   const movies = await getMovives();
   return <div>
-    <div>{JSON.stringify(movies)}</div>
+    <div>{
+      movies.map(movie => <li key={movie.id}><Link href={`movies/${movie.id}`}>{movie.title}</Link></li>)
+      }</div>
   </div>
 }
