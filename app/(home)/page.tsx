1856384fa708287c67814da44f446c1bddcc01a1
Areum An <areum.an.0928@gmail.com>
2024-10-14 12:49:05 -0400

    fetch - old way

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,24 @@
+"use client"
 
-export const metadata = {
-  title: 'Home'
-}
+import { useEffect, useState } from 'react';
+
+// export const metadata = {
+//   title: 'Home'
+// }
 
 export default function Page() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    getMovies();
+  },)
   return <div>
-    <h1>Main!</h1>
+    <div>{isLoading ?  "Loading..." : JSON.stringify(movies)}</div>
   </div>
 }
